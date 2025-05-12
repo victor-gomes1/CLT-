dbf8ecfdf4788642307eb0e206b002c80b2a7362
Victor Henrique <victorrherique@hotmail.com>
2025-05-12 11:38:41 +0000

    atividade2

--- CLT/Atividade2/CSS/script.js
+++ CLT/Atividade2/CSS/script.js
@@ -4,13 +4,13 @@ document.addEventListener('DOMContentLoaded', function () {
     const secaoCurriculo = document.getElementById('curriculo');
     const secaoFormulario = document.getElementById('formulario');
 
-    // Ir para o formulário
+    
     btnFormulario.addEventListener('click', () => {
         secaoCurriculo.style.display = 'none';
         secaoFormulario.style.display = 'block';
     });
 
-    // Voltar para o currículo
+    
     btnVoltar.addEventListener('click', () => {
         secaoFormulario.style.display = 'none';
         secaoCurriculo.style.display = 'block';
